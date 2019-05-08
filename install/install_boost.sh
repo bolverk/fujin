00214a6c30d6d9215568c7c1ee65e02c1cc47758
Almog Yalinewich <almog.yalin@gmail.com>
2019-05-08 17:03:08 -0400

    add script to install boost

---
+++ install/install_boost.sh
@@ -0,0 +1,6 @@
+cd $FUJIN_ROOT
+mkdir -p external_libraries
+cd external_libraries
+wget http://sourceforge.net/projects/boost/files/boost/1.66.0/boost_1_66_0.tar.bz2/download 
+mv ./download ./boost_1_66_0.tar.bz2
+tar xf ./boost_1_66_0.tar.bz2
