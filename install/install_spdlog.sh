cd789afed03b99df3eb2c63c659701a3dfe75dcc
Almog Yalinewich <almog.yalin@gmail.com>
2020-04-10 20:18:55 -0400

    remove compilation

--- install/install_spdlog.sh
+++ install/install_spdlog.sh
@@ -2,5 +2,5 @@ cd $FUJIN_ROOT
 mkdir -p external_libraries
 cd external_libraries
 git clone https://github.com/gabime/spdlog.git
-cd spdlog && mkdir build && cd build
-cmake .. && make -j
+#cd spdlog && mkdir build && cd build
+#cmake .. && make -j
