a2c97276adb1d970dcc1d881a1ab6d8d7cb67f10
Almog Yalinewich <almog.yalin@gmail.com>
2020-08-25 18:37:26 -0400

    add rerun test

---
+++ tests/srhd_sim/rerun/test.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "srhd_sim.hpp"
+#include "pcm.hpp"
+#include "ideal_gas.hpp"
+#include "hll.hpp"
+#include "rigid_wall.hpp"
+#include "hdf5_snapshot.hpp"
+#include "universal_error.hpp"
+#include "main_loop.hpp"
+#ifdef PARALLEL
+#include "parallel_helper.hpp"
+#endif // PARALLEL
+
+using namespace std;
+
+namespace {
+
+  void main_loop(SRHDSimulation& sim,
+		 const double tf,
+		 const string& outname)
+  {
+    SafeTimeTermination term_cond(tf, 1e5);
+    WriteTime diag("time.txt");
+    main_loop(sim,
+	      term_cond,
+	      &SRHDSimulation::TimeAdvance,
+	      diag);
+#ifdef PARALLEL
+    write_hdf5_snapshot(sim,"final_"+int2str(get_mpi_rank())+".h5");
+#else
+    write_hdf5_snapshot(sim,outname);
+#endif // PARALLEL
+  }
+}
+
+class SimData
+{
+public:
+
+  SimData(void):
+    adiabatic_index_(5./3.),
+    edges_(linspace(0,1,100)),
+    density_(10,1,0.5),
+    pressure_(13.33,1e-8,0.5),
+    velocity_(0),
+    eos_(adiabatic_index_),
+    rs_(eos_),
+    sr_(),
+    bc_(rs_),
+    geometry_(),
+    sim_(edges_,
+	 density_,
+	 pressure_,
+	 velocity_,
+	 bc_, bc_,
+	 eos_, rs_,
+	 sr_, 
+	 geometry_) {}
+
+  SimData(const string& fname):
+    adiabatic_index_(5./3.),
+    edges_(linspace(0,1,100)),
+    density_(10,1,0.5),
+    pressure_(13.33,1e-8,0.5),
+    velocity_(0),
+    eos_(adiabatic_index_),
+    rs_(eos_),
+    sr_(),
+    bc_(rs_),
+    geometry_(),
+    sim_(read_hdf5_snapshot(fname),
+	 bc_, bc_,
+	 eos_, rs_,
+	 sr_, 
+	 geometry_) {}
+
+  SRHDSimulation& getSim(void)
+  {
+    return sim_;
+  }
+
+private:
+  double adiabatic_index_;
+  vector<double> edges_;
+  Step density_;
+  Step pressure_;
+  Uniform velocity_;
+  IdealGas eos_;
+  //  IdealGasRiemannSolver rs_;
+  HLL rs_;
+  PCM sr_;
+  RigidWall bc_;
+  const Planar geometry_;
+  SRHDSimulation sim_;
+};
+
+namespace {
+
+  void main_loop_weh(SRHDSimulation& sim,
+		     const double tf,
+		     const string& outname)
+  {
+    try{
+      main_loop(sim, tf, outname);
+    }
+    catch(UniversalError const& eo){
+      report_error(eo,cout);
+      throw;
+    }
+  }
+
+}
+
+int main()
+{
+#ifdef PARALLEL
+  MPI_Init(NULL, NULL);
+  throw("not implemented yet");
+#endif // PARALLEL
+  {
+    SimData sim_data;
+    SRHDSimulation& sim = sim_data.getSim();
+
+    main_loop_weh(sim, 0.4, "continuous.h5");
+  }
+
+  {
+    SimData sim_data;
+    SRHDSimulation& sim = sim_data.getSim();
+
+    main_loop_weh(sim, 0.2, "checkpoint.h5");
+  }
+
+  {
+    SimData sim_data("checkpoint.h5");
+    SRHDSimulation& sim = sim_data.getSim();
+
+    main_loop_weh(sim, 0.2, "interrupted.h5");
+  }
+  
+  ofstream("test_terminated_normally.res").close();
+
+#ifdef PARALLEL
+  MPI_Finalize();
+#endif // PARALLEL
+
+  return 0;
+}
---
+++ tests/srhd_sim/rerun/test.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python
+
+def calc_l1(a1, a2):
+
+    import numpy
+    return sum(numpy.abs(numpy.array(a1)-numpy.array(a2)))/len(a1)
+
+def consolidate(fname):
+
+    import h5py
+    import numpy
+
+    with h5py.File(fname,'r') as f:
+        data = dict((key,numpy.array(f[key])) for key in f)
+    return data
+
+def consolidate_all(pattern):
+
+    from glob import glob
+    import re
+    import numpy
+
+    file_list = sorted(glob(pattern),
+                       key=lambda fname:int(re.search('(\d+)',fname)[0]))
+    partitions = [consolidate(fname) for fname in file_list]
+    return {field:numpy.concatenate([part[field] for part in partitions])
+            for field in partitions[0]}
+
+def main():
+
+    import numpy
+    import os
+    import sys
+    assert('FUJIN_ROOT' in os.environ)
+    sys.path.append(os.environ['FUJIN_ROOT']+'/analytic')
+    from ideal_gas_riemann_solver import RiemannProfile, Primitive
+    from glob import glob
+
+    continuous = consolidate('continuous.h5');
+    interrupted = consolidate('interrupted.h5')
+    checkpoint = consolidate('checkpoint.h5')
+
+    l1_thresholds = {}
+
+    for field in ['edges', 'density', 'pressure', 'celerity']:
+        if calc_l1(continuous[field],
+                   interrupted[field])>1e-2:
+            return False    
+
+    return True
+
+if __name__=='__main__':
+    
+    import os
+
+    os.system('rm -rf test_passed.res test_failed.res')
+    if main():
+        os.system('touch test_passed.res')
+    else:
+        os.system('touch test_failed.res')
