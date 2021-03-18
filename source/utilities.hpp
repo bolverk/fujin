37700ea6e468dd9263485cb15b30b9209474c129
Almog Yalinewich <almog.yalin@gmail.com>
2021-03-17 21:40:31 -0400

    remove unused function

--- source/utilities.hpp
+++ source/utilities.hpp
@@ -297,6 +297,7 @@ template<class S, class T> vector<T> operator*
   \param i2m Lazy list
   \return Minimum term
  */
+/*
 template<class T> T min(const Index2Member<T>& i2m)
 {
   T res = i2m(0);
@@ -304,5 +305,6 @@ template<class T> T min(const Index2Member<T>& i2m)
     res = min(res,i2m(i));
   return res;
 }
+*/
 
 #endif // UTILITIES_HPP
