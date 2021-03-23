d5fe97bc73a386eb96b1a1d9825bfc2346f4b1b3
Almog Yalinewich <almog.yalin@gmail.com>
2021-03-23 15:33:30 -0400

    scaffolding

--- source/srhd_sim.hpp
+++ source/srhd_sim.hpp
@@ -1,7 +1,7 @@
 /*! \file srhd_sim.hpp
   \brief Relativistic, Godunov type, lagrangian 1d hydrodynamic simulation
   \author Almog Yalinewich
- */
+*/
 
 #ifndef SRHD_SIM_HPP
 #define SRHD_SIM_HPP 1
@@ -22,8 +22,8 @@
 #endif // SCAFFOLDING
 
 #if SCAFFOLDING != 1
-double new_calc_time_step
-(const NewHydroSnapshot<vector<double>, vector<Primitive> >& data,
+template<class CE, class CP> double new_calc_time_step
+(const NewHydroSnapshot<CE, CP>& data,
  const vector<RiemannSolution>& psvs,
  const EquationOfState& eos,
  const double cfl)
@@ -46,296 +46,266 @@ double new_calc_time_step
   return cfl/its;
 }
 
-  vector<double> distribute_vertices1
-    (const vector<double>& vertices)
-  {
-#ifdef PARALLEL
-    spdlog::debug("Inside parallel distribute vertices");
-    const size_t cell_num = vertices.size()-1;
-    const int rank = get_mpi_rank();
-    const int size = get_mpi_size();
-    spdlog::debug("cell_num {0}, rank {1}, size {2}",
-		  cell_num, rank, size);
-    
-    vector<int> partition(static_cast<size_t>(size), cell_num/size);
-    for(size_t i=0;i<cell_num%size;++i)
-      ++partition.at(i);
-    
-    vector<int> cumpar(partition.size()+1,0);
-    for(size_t i=1;i<cumpar.size();++i)
-      cumpar.at(i) = cumpar.at(i-1) + partition.at(i-1);
+vector<double> distribute_vertices1
+(const vector<double>& vertices);
 
-    const size_t low = cumpar.at(rank);
-    const size_t high = cumpar.at(rank+1);
-
-    vector<double> res(high-low+1,0);
-    for(size_t i=0;i<res.size();++i)
-      res.at(i) = vertices.at(low+i);
-    spdlog::debug("low {0}, high {1}",
-		  res.front(),
-		  res.back());
-    return res;
-#else
-    return vertices;
-#endif // PARALLEL
-  }
 #endif // SCAFFOLDING
 
 /*! \brief Special relativistic hydrodynamic simulation
   \details The simulation is based on <a href="http://adsabs.harvard.edu/abs/2000A%26A...358.1157D"> F. Daigne & R. Moskovitch, "Gamma-ray bursts from internal shocks in a relativistic wind: a hydrodynamical study", A&A, v. 358 p 1157-1166 (2000) </a>
- */
+*/
 #if SCAFFOLDING == 1
 class SRHDSimulation
 #else
 template<class CE, class CP> class SRHDSimulation
 #endif // SCAFFOLDING
-{
-private:
-  //! \brief Hydrodynamic state (edges and cells)
-  NewHydroSnapshot<vector<double>, vector<Primitive> > data_;
-  //! \brief Equation of state
-  const EquationOfState& eos_;
-  //! \brief Riemann solutions
-  vector<RiemannSolution> psvs_;
-  //! \brief rs Riemann sovler
-  const RiemannSolver& rs_;
-  //! \brief Spatial reconstruction method
-  const SpatialReconstruction& sr_;
-  //! \brief Courant Friedrichs Lewy factor
-  double cfl_;
-  //! \brief Vectors of conserved variables
-  vector<NewConserved> consVars_;
-  //! \brief Rest masses of the cells
-  vector<double> restMass_;
-  //! \brief Geometry
-  const Geometry& geometry_;
-  //! \brief Virtual time
-  double time_;
-  //! \brief Cycle number
-  int cycle_;
-  //! \brief Innter boundary condition
-  const BoundaryCondition& innerBC_;
-  //! \brief Outer boundary condition
-  const BoundaryCondition& outerBC_;
+  {
+  private:
+    //! \brief Hydrodynamic state (edges and cells)
+    NewHydroSnapshot<vector<double>, vector<Primitive> > data_;
+    //! \brief Equation of state
+    const EquationOfState& eos_;
+    //! \brief Riemann solutions
+    vector<RiemannSolution> psvs_;
+    //! \brief rs Riemann sovler
+    const RiemannSolver& rs_;
+    //! \brief Spatial reconstruction method
+    const SpatialReconstruction& sr_;
+    //! \brief Courant Friedrichs Lewy factor
+    double cfl_;
+    //! \brief Vectors of conserved variables
+    vector<NewConserved> consVars_;
+    //! \brief Rest masses of the cells
+    vector<double> restMass_;
+    //! \brief Geometry
+    const Geometry& geometry_;
+    //! \brief Virtual time
+    double time_;
+    //! \brief Cycle number
+    int cycle_;
+    //! \brief Innter boundary condition
+    const BoundaryCondition& innerBC_;
+    //! \brief Outer boundary condition
+    const BoundaryCondition& outerBC_;
 
-public:
-  /*! \brief Class constructor
-    \param vertices List of vertices
-    \param density_distribution Initial density distribution
-    \param pressure_distribution Initial pressure distribution
-    \param proper_velocity_distribution Initial proper velocity distribution
-    \param inner_bc Inner boundary conditions
-    \param outer_bc Outer boundary conditions
-    \param eos Equation of state
-    \param riemann_solver Reference to riemann solver
-    \param interpolation_method Pointer to spatial reconstruction
-    \param geometry Geometry
-   */
-  SRHDSimulation(const vector<double>& vertices,
-		 const SpatialDistribution& density_distribution,
-		 const SpatialDistribution& pressure_distribution,
-		 const SpatialDistribution& proper_velocity_distribution,
-		 const BoundaryCondition& inner_bc,
-		 const BoundaryCondition& outer_bc,
-		 const EquationOfState& eos,
-		 const RiemannSolver& riemann_solver,
-		 const SpatialReconstruction& interpolation_method,
-		 const Geometry& geometry)
+  public:
+    /*! \brief Class constructor
+      \param vertices List of vertices
+      \param density_distribution Initial density distribution
+      \param pressure_distribution Initial pressure distribution
+      \param proper_velocity_distribution Initial proper velocity distribution
+      \param inner_bc Inner boundary conditions
+      \param outer_bc Outer boundary conditions
+      \param eos Equation of state
+      \param riemann_solver Reference to riemann solver
+      \param interpolation_method Pointer to spatial reconstruction
+      \param geometry Geometry
+    */
+    SRHDSimulation(const vector<double>& vertices,
+		   const SpatialDistribution& density_distribution,
+		   const SpatialDistribution& pressure_distribution,
+		   const SpatialDistribution& proper_velocity_distribution,
+		   const BoundaryCondition& inner_bc,
+		   const BoundaryCondition& outer_bc,
+		   const EquationOfState& eos,
+		   const RiemannSolver& riemann_solver,
+		   const SpatialReconstruction& interpolation_method,
+		   const Geometry& geometry)
 #if SCAFFOLDING == 1
-    ;
+      ;
 #else
-  :
-  data_(distribute_vertices1(vertices),
-	InitCells(distribute_vertices1(vertices),
-		  density_distribution,
-		  pressure_distribution,
-		  proper_velocity_distribution)),
-  eos_(eos), 
-  psvs_(distribute_vertices1(vertices).size(),
-	RiemannSolution()),
-  rs_(riemann_solver), 
-  sr_(interpolation_method),
-  cfl_(1./3.), 
-  consVars_(primitives_to_new_conserveds(data_.cells,eos)),
-  restMass_(serial_generate(RestMassCalculator(data_,geometry))),
-  geometry_(geometry),
-  time_(0),
-  cycle_(0),
-  innerBC_(inner_bc), 
-  outerBC_(outer_bc) {}
+    :
+    data_(distribute_vertices1(vertices),
+	  InitCells(distribute_vertices1(vertices),
+		    density_distribution,
+		    pressure_distribution,
+		    proper_velocity_distribution)),
+      eos_(eos), 
+      psvs_(distribute_vertices1(vertices).size(),
+	    RiemannSolution()),
+      rs_(riemann_solver), 
+      sr_(interpolation_method),
+      cfl_(1./3.), 
+      consVars_(primitives_to_new_conserveds(data_.cells,eos)),
+      restMass_(serial_generate(RestMassCalculator(data_,geometry))),
+      geometry_(geometry),
+      time_(0),
+      cycle_(0),
+      innerBC_(inner_bc), 
+      outerBC_(outer_bc) {}
 #endif // SCAFFOLDING
 
-  /*! \brief Class constructor
-    \param init_cond Initial conditions
-    \param inner_bc Inner boundary conditions
-    \param outer_bc Outer boundary conditions
-    \param eos Equation of state
-    \param rs Riemann solver
-    \param interpolation_method Pointer to spatial reconstruction
-    \param geometry Geometry
-   */
-  SRHDSimulation
-  (const NewHydroSnapshot<vector<double>, vector<Primitive> >& init_cond,
-   const BoundaryCondition& inner_bc,
-   const BoundaryCondition& outer_bc,
-   const EquationOfState& eos,
-   const RiemannSolver& rs,
-   const SpatialReconstruction& interpolation_method,
-   const Geometry& geometry);
+    /*! \brief Class constructor
+      \param init_cond Initial conditions
+      \param inner_bc Inner boundary conditions
+      \param outer_bc Outer boundary conditions
+      \param eos Equation of state
+      \param rs Riemann solver
+      \param interpolation_method Pointer to spatial reconstruction
+      \param geometry Geometry
+    */
+    SRHDSimulation
+      (const NewHydroSnapshot<vector<double>, vector<Primitive> >& init_cond,
+       const BoundaryCondition& inner_bc,
+       const BoundaryCondition& outer_bc,
+       const EquationOfState& eos,
+       const RiemannSolver& rs,
+       const SpatialReconstruction& interpolation_method,
+       const Geometry& geometry);
   
-  //! \brief Advances the simulation in time
-  void timeAdvance1stOrder(void);
+    //! \brief Advances the simulation in time
+    void timeAdvance1stOrder(void);
 
-  //! \brief Advances the simulation in time
-  void timeAdvance2ndOrder(void);
+    //! \brief Advances the simulation in time
+    void timeAdvance2ndOrder(void);
 
-  //! \brief Advances the simulation in time
+    //! \brief Advances the simulation in time
 #if SCAFFOLDING == 1
-  void timeAdvance(void);
+    void timeAdvance(void);
 #else
-  void timeAdvance(void)
-{
-  CalcFluxes(data_,
-	     sr_,rs_,0,
-	     innerBC_,
-	     outerBC_,
-	     psvs_);
+    void timeAdvance(void)
+    {
+      CalcFluxes(data_,
+		 sr_,rs_,0,
+		 innerBC_,
+		 outerBC_,
+		 psvs_);
 
 #ifdef PARALLEL
-  const double dt_candidate =
-    new_calc_time_step(data_,
-		       psvs_,
-		       eos_,
-		       cfl_);
-  double temp = dt_candidate;
-  MPI_Allreduce(&dt_candidate,
-		&temp,
-		1,
-		MPI_DOUBLE,
-		MPI_MIN,
-		MPI_COMM_WORLD);
-  const double dt = temp;
-  spdlog::debug("dt = {0}", dt);
+      const double dt_candidate =
+	new_calc_time_step(data_,
+			   psvs_,
+			   eos_,
+			   cfl_);
+      double temp = dt_candidate;
+      MPI_Allreduce(&dt_candidate,
+		    &temp,
+		    1,
+		    MPI_DOUBLE,
+		    MPI_MIN,
+		    MPI_COMM_WORLD);
+      const double dt = temp;
+      spdlog::debug("dt = {0}", dt);
 #else
-  const double dt = new_calc_time_step(data_,
-				       psvs_,
-				       eos_,
-				       cfl_);
+      const double dt = new_calc_time_step(data_,
+					   psvs_,
+					   eos_,
+					   cfl_);
 #endif // PARALLEL
 
-  update_new_conserved(psvs_,
-		       data_.cells,
-		       restMass_,
-		       dt, geometry_,
-		       data_.edges,
-		       consVars_);
+      update_new_conserved(psvs_,
+			   data_.cells,
+			   restMass_,
+			   dt, geometry_,
+			   data_.edges,
+			   consVars_);
 
-  for(size_t i=1;i<data_.edges.size();++i)
-    assert(data_.edges[i]>data_.edges[i-1]);
-  for_each(consVars_.begin(),
-	   consVars_.end(),
-	   [](const NewConserved& cv)
-	   {assert(cv.mass>0);});
+      for(size_t i=1;i<data_.edges.size();++i)
+	assert(data_.edges[i]>data_.edges[i-1]);
+      for_each(consVars_.begin(),
+	       consVars_.end(),
+	       [](const NewConserved& cv)
+	       {assert(cv.mass>0);});
 
-  const vector<bool> filter = NeedUpdate(psvs_);
+      const vector<bool> filter = NeedUpdate(psvs_);
 
-  UpdatePrimitives(consVars_, eos_, filter, data_.cells);
+      UpdatePrimitives(consVars_, eos_, filter, data_.cells);
 
-  time_ += dt;
-  cycle_++;
-}
+      time_ += dt;
+      cycle_++;
+    }
 #endif // SCAFFOLDING
 
-  /*! \brief Calculates the conserved variables from the primitives 
-   */
-  void calcConservedFromPrimitive(void);
+    /*! \brief Calculates the conserved variables from the primitives 
+     */
+    void calcConservedFromPrimitive(void);
 
-  /*! \brief override default Courant Friedrichs Lewy coefficient
-    \details Determins the ratio between the actual time step and the maximal time step
-    \param cfl_new New CFL coefficient
-   */
-  void overrideCFL(double cfl_new);
+    /*! \brief override default Courant Friedrichs Lewy coefficient
+      \details Determins the ratio between the actual time step and the maximal time step
+      \param cfl_new New CFL coefficient
+    */
+    void overrideCFL(double cfl_new);
 
-  // Diagnostics
+    // Diagnostics
 
-  /*! \brief Calculates the time step for a certain cell
-    \param Index Cell index
-    \return Time step
-  */
-  double calcTimeStepForCell(size_t Index) const;
+    /*! \brief Calculates the time step for a certain cell
+      \param Index Cell index
+      \return Time step
+    */
+    double calcTimeStepForCell(size_t Index) const;
 
-  /*! \brief Return the volume bounded by a certain vertex
-    \param Index Vertex index
-    \return Volume
-  */
-  double getVolume(size_t Index) const;
+    /*! \brief Return the volume bounded by a certain vertex
+      \param Index Vertex index
+      \return Volume
+    */
+    double getVolume(size_t Index) const;
 
-  /*! \brief Returns the hydrodynamic snapshot
-    \return Hydrodynamic snapshot
-   */
-  const decltype(data_)& getHydroSnapshot(void) const
+    /*! \brief Returns the hydrodynamic snapshot
+      \return Hydrodynamic snapshot
+    */
+    const decltype(data_)& getHydroSnapshot(void) const
 #if SCAFFOLDING == 1
-    ;
+      ;
 #else
-  {
-    return data_;
-  }
+    {
+      return data_;
+    }
 #endif // SCAFFOLDING
 
-  /*! \brief Returns a list of Riemann solutions
-    \return Riemann solutions
-   */
-  const vector<RiemannSolution>& getRiemannSolutions(void) const;
+    /*! \brief Returns a list of Riemann solutions
+      \return Riemann solutions
+    */
+    const vector<RiemannSolution>& getRiemannSolutions(void) const;
 
-  /*! \brief Returns the conserved variables
-    \return Conserved variables
-   */
-  const vector<NewConserved>& getConserved(void) const;
+    /*! \brief Returns the conserved variables
+      \return Conserved variables
+    */
+    const vector<NewConserved>& getConserved(void) const;
 
-  /*! \brief Calculates the time step
-    \return Time step
-  */
-  double calcTimeStep(void) const;
+    /*! \brief Calculates the time step
+      \return Time step
+    */
+    double calcTimeStep(void) const;
 
-  /*! \brief Calculates the area at a certain cell centre
-    \param Index Cell index
-    \return Area
-   */
-  double getArea(size_t Index) const;
+    /*! \brief Calculates the area at a certain cell centre
+      \param Index Cell index
+      \return Area
+    */
+    double getArea(size_t Index) const;
 
-  /*! \brief Returns the time
-  \return Time
-  */
+    /*! \brief Returns the time
+      \return Time
+    */
 #if SCAFFOLDING == 1
-  double getTime(void) const;
+    double getTime(void) const;
 #else
-  double getTime(void) const
-  {
-  return time_;
-  }
+    double getTime(void) const
+    {
+      return time_;
+    }
 #endif // SCAFFOLDING
 
-  /*! \brief Returns cycle number
-    \return Cycle number
-   */
+    /*! \brief Returns cycle number
+      \return Cycle number
+    */
 #if SCAFFOLDING == 1
-  int getCycle(void) const;
+    int getCycle(void) const;
 #else
-  int getCycle(void) const
-  {
-    return cycle_;
-  }
+    int getCycle(void) const
+    {
+      return cycle_;
+    }
 #endif // SCAFFOLDING
 
-  /*! \brief Returns the rest masses
-    \return Rest masses
-   */
-  const vector<double>&  getRestMasses(void) const;
+    /*! \brief Returns the rest masses
+      \return Rest masses
+    */
+    const vector<double>&  getRestMasses(void) const;
 
-  /*! \brief Returns the equation of state
-    \return Equation of state
-   */
-  const EquationOfState& getEOS(void) const;
-};
+    /*! \brief Returns the equation of state
+      \return Equation of state
+    */
+    const EquationOfState& getEOS(void) const;
+  };
 
 #endif
